d7f1f102b2dd72aa32d6df34a695c0b6d591604b
Dmitrii Sakharov <sahappleid@yandex.ru>
2025-09-10 00:16:26 +0200

    changed structure

--- start_bash_scripts/start_postgres.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker run --name jervis_db --rm \
--e POSTGRES_PASSWORD=password -e POSTGRES_USER=postgres -e POSTGRES_DB=postgres \
--v jervis_postgres_vol:/var/lib/postgresql/data \
--p 5432:5432 -d postgres
